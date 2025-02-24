05d04e5b2eaceca81c1b9edb3022bf757dbaf988
Steve Green <121233991+steveygfromao@users.noreply.github.com>
2025-02-24 11:04:29 +0000

    Added state

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
 
 import './App.css'
+import { useState, useEffect } from 'react'
 import PlayerDisplayTable from './components/PlayerDisplayTable'
 import playersList  from './models/players'
 
 const App = () => {
 
   const PlayerAmount = playersList.length;
+  const [playerTeamOne, setTeamOnePlayers] = useState([]);
+  const [playerTeamTwo, setTeamTwoPlayers] = useState([]);
 
+  useEffect(() => {
+    recreateTeams();
+  }, []);
+  
   const createTeams = (players) => {
     for (let i = players.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [players[i], players[j]] = [players[j], players[i]];
     }
-    return players;
+   
+    setTeamOnePlayers(players.slice(0, PlayerAmount/2));
+    setTeamTwoPlayers(players.slice(PlayerAmount/2, PlayerAmount));
+
   }
 
-  const shuffledPlayers = createTeams([...playersList]);
-  const team1 = shuffledPlayers.slice(0, PlayerAmount/2);
-  const team2 = shuffledPlayers.slice(PlayerAmount/2, PlayerAmount);
-  
+  const recreateTeams = () => {
+    createTeams([...playersList]);  // will do a refresh as stored in state
+  };
+
   return (
     <>
       <h1>Football Team Selector</h1>
       <div>
           <h2>Team 1</h2>
-          <PlayerDisplayTable players={team1} />
+          <PlayerDisplayTable players={playerTeamOne} />
           <h2>Team 2</h2>
-          <PlayerDisplayTable players={team2} />
+          <PlayerDisplayTable players={playerTeamTwo} />
+      </div>
+      <div>
+        <button class="button" onClick={recreateTeams}>Re-create</button>
       </div>
     </>
   );
