330553bd04fa48d5fda86f4d0184ad0f859e3643
Steve Green <121233991+steveygfromao@users.noreply.github.com>
2025-04-02 14:18:42 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import playersList  from './models/players'
 
 const App = () => {
 
-  const SKILL_THRESHOLD = 0.6;
+  const SKILL_THRESHOLD = 0.4;
   const PlayerAmount = playersList.length;
   const [playerTeamOne, setTeamOnePlayers] = useState([]);
   const [playerTeamTwo, setTeamTwoPlayers] = useState([]);
