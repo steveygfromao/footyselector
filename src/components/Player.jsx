976cc514dbacb68f044e7497c85dfee3c72df050
Steve Green <121233991+steveygfromao@users.noreply.github.com>
2025-04-02 15:49:07 +0100

    Add files via upload

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,11 @@
 
 const Player = ({playerObj}) => {
+    console.log(playerObj.image)
     return (
         <>
             <td>{playerObj.name}</td>
             <td>{playerObj.position}</td>
+            <td><img src={playerObj.image} width="24px" alt="a cool image" loading="lazy" /></td>
          {/*   <td>{playerObj.skill}</td> */}
         </>
     );
